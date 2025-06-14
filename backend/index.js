8e66d75c70cd133f117b0837dcd88c4b2b9b9c90
Deepak Kumar Yadav <dky422003@gmail.comm>
2025-06-14 22:28:47 +0530

    deployment ready

--- backend/index.js
+++ backend/index.js
@@ -39,12 +39,7 @@ const __dirname = path.resolve();
 const app = express();
 
 // Configure CORS with specific options
-app.use(cors({
-    origin: 'http://localhost:5173', // Frontend URL
-    credentials: true, // Allow credentials (cookies, authorization headers, etc)
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
