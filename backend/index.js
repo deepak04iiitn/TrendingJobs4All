68f7494e6979208bc0cce24c94abf6c54d12d513
Deepak Kumar Yadav <dky422003@gmail.comm>
2025-07-13 14:23:34 +0530

    fixed port

--- backend/index.js
+++ backend/index.js
@@ -238,6 +238,7 @@ if (process.argv[2] === 'run-cron') {
 }
 
 // Start the server
-app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}!`);
 });
